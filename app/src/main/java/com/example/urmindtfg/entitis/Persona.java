7d955fc2874c47ffedf1f044ab0306dbc71baf2c
UnroPrograming <unrrotundo@gmail.com>
2022-05-04 22:18:15 +0200

    Clase Persona

---
+++ app/src/main/java/com/example/urmindtfg/entitis/Persona.java
@@ -0,0 +1,85 @@
+package com.example.urmindtfg.entitis;
+
+import java.util.HashMap;
+
+public class Persona {
+    private String email;
+    private String nombre;
+    private String apellidos;
+    private int telefono;
+    private String DNI;
+    private String proveedor;''
+
+    public Persona() {
+    }
+
+    public Persona(String email, String nombre, String apellidos, int telefono, String DNI, String proveedor) {
+        this.email = email;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.telefono = telefono;
+        this.DNI = DNI;
+        this.proveedor = proveedor;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(String DNI) {
+        this.DNI = DNI;
+    }
+
+    public String getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(String proveedor) {
+        this.proveedor = proveedor;
+    }
+
+    public HashMap<String,Object> toHashMap(){
+        HashMap<String,Object> lista = new HashMap<String,Object>();
+
+        lista.put("email",getEmail());
+        lista.put("nombre",getNombre());
+        lista.put("apellidos",getApellidos());
+        lista.put("telefono",getTelefono());
+        lista.put("dni",getDNI());
+        lista.put("proveedor",getProveedor());
+
+        return lista;
+    }
+}
