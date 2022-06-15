cedb667be53b9863016711140afc52e2b2066554
danimamblona <dme2202@gmail.com>
2022-06-16 01:05:22 +0200

    ubicacion atual

--- app/src/main/java/com/example/urmindtfg/ui/ubicaciones/UbicacionesFragment.java
+++ app/src/main/java/com/example/urmindtfg/ui/ubicaciones/UbicacionesFragment.java
@@ -1,5 +1,8 @@
 package com.example.urmindtfg.ui.ubicaciones;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,10 +10,13 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.urmindtfg.R;
 import com.example.urmindtfg.databinding.FragmentUbicacionesBinding;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -18,11 +24,55 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import org.androidannotations.api.BackgroundExecutor;
 
 public class UbicacionesFragment extends Fragment {
     private FragmentUbicacionesBinding binding;
+    public SupportMapFragment supportMapFragment;
+    public FusedLocationProviderClient client;
+
     public static UbicacionesFragment newInstance() {
         return new UbicacionesFragment();
+    }
+
+    public void getCurrentLocation() {
+        //Initalize task location
+        if (ActivityCompat.checkSelfPermission(getActivity().getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity().getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Task<Location> task = client.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                //When succes
+                if (location != null){
+                    //Sync map
+                    supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+                        @Override
+                        public void onMapReady(@NonNull GoogleMap googleMap) {
+                            //Initialice lat lang
+                            LatLng latLng = new LatLng(location.getLatitude()
+                                    ,location.getLongitude());
+                            //Create marker options
+                            MarkerOptions options = new MarkerOptions().position(latLng)
+                                    .title("Estas aqui");
+                            //Add marker on map
+                            googleMap.addMarker(options);
+                        }
+                    });
+                }
+            }
+        });
     }
 
     @Override
@@ -63,7 +113,21 @@ public class UbicacionesFragment extends Fragment {
             }
         });
 
+        //Initialize fused location
+        client = LocationServices.getFusedLocationProviderClient(getActivity().getApplicationContext());
 
+        //Check permission
+        if(ActivityCompat.checkSelfPermission(getActivity().getApplicationContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            //When permission grated
+            //Cal method
+            getCurrentLocation();
+        }else{
+            //Wen permission denied
+            //Request permission
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+        }
 
         //Return view
         return view;
